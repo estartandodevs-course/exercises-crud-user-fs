2b1225d89c9c69eb1a0fa93fb09689d32aa8fbb0
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-06-18 16:14:46 -0300

    updating files

--- src/services/users/create-user.service.js
+++ src/services/users/create-user.service.js
@@ -1,11 +1,34 @@
+const { loadUsersRepository,  createUserRepository,  } = require('../../repositories/user-repository')
+
+
 async function createUser({ name, email, password, phone }) {
   /*
-  - TODO 15: Os campos name, email, password são obrigatórios, caso algum não seja passado deve retornar uma exceção de error "Name, Email, Password are required";
-  - TODO 16: Se o email passado já existir no banco para outro usuário, deve retornar uma exceção com o erro "User already exists";
-  - TODO 17: Deve gerar o ID automaticamente a partir de 1000;
   - TODO 18: Deve ser usado a funcionalidade "encryptPassword" para proteger o password;
-  - TODO 19: Deve retornar um usuário válido ao final do processo de insert;
 */
+  if ( name == null || email  == null || phone  == null) {throw new Error('Name, Email, Password are required')};
+
+  const usersRepository = await loadUsersRepository()
+  
+  const newId = new Date().getTime();
+  
+  const user = {
+    id: newId,
+    name: name,
+    email: email,
+    passwd: password,
+    phone: phone,
+    status: true
+  }
+  
+  usersRepository.forEach(element => {
+    const {email} = element
+    if (user.email == email) {throw new Error('User already exists')}
+  });
+  
+  createUserRepository(user)
+  return user;
 }
+
+
 
 module.exports = { createUser };
--- src/services/users/delete-user.service.js
+++ src/services/users/delete-user.service.js
@@ -1,10 +1,16 @@
-async function deleteUser(id) {
-  /*
+const { loadUsersRepository,  deleteUserRepository,  } = require('../../repositories/user-repository')
 
-  - TODO 20: Deve retornar uma exceção de erro "User ID is required" se o ID não for passado;
-  - TODO 21: Se pelo ID passado não existir um usuário no banco, deve retornar uma exceção com o erro "User not exists";
-  - TODO 22: Com o sucesso na remoção, deve retornar TRUE
-*/
+async function deleteUser(id) {
+
+  if (id == null) throw new Error('User ID is required');
+
+  const usersRepository = await loadUsersRepository()
+
+  const user = usersRepository.filter((obj) => obj.id == id)
+  if (user[0] == null) throw new Error('User not exists')
+  
+  return deleteUserRepository(id)
+
 }
 
 module.exports = { deleteUser };
