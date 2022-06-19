53446acdb9b320582050d9fafe43bedb0b033faa
b1z3rr4 <nataliasilva.bezerra18@gmail.com>
2022-06-19 19:00:47 -0300

    update the indentation!

--- src/utils/encrypt-password.js
+++ src/utils/encrypt-password.js
@@ -5,11 +5,9 @@ function encryptPassword(password) {
   if(!password){
     throw new Error("Password is required");
   }
-
   if(password.length < 8){
     throw new Error("Password must be at least 8 characters");
   }
-
   const salt = process.env.SALT;
   const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
   return hash;
