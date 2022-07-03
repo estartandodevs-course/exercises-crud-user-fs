c4d6ca8453c5cc1a235f3719138b07fe561c89b1
Karine <karinevitoria.marinho@gmail.com>
2022-07-03 12:20:28 -0300

    search user by name

--- src/services/users/search-users.service.js
+++ src/services/users/search-users.service.js
@@ -1,7 +1,16 @@
+const {loadUsersRepository} = require ("../../repositories/user-repository");
+
 async function searchUsers(name) {
   /*
   - TODO 28: Deve retornar a lista de usuários válidos filtrado por nome;
 */
+  const usersList = await loadUsersRepository();
+
+  let userListFilterByName = usersList.filter(data => 
+    data.name.toString() === name
+  );
+
+  return userListFilterByName;
 }
 
 module.exports = { searchUsers };
