63e87221a790ab2108498efc7858c6ede5bf0bba
Victor Almeida <victorpy.1999@gmail.com>
2022-07-04 09:00:12 -0300

    feat: updating repositories

--- src/repositories/user-repository.js
+++ src/repositories/user-repository.js
@@ -1,35 +1,43 @@
-/*
-  IMPORTANTE! A responsabilidade do Repository é apenas a gerencia dos dados
-  Não deve conter regras de negócio!
-*/
+const fs = require('fs')
+const path = require('path')
+const getDirectory = path.join(__dirname, '..', 'data', 'users.json')
+
 
 async function loadUsersRepository() {
-  /*
-  - TODO 1 : Deve retornar uma lista de usuários a partir do caminho src/data/users.json;
-  - TODO 2 : Deve ser usado um método de leitura do FS como readFile nessa implementação;  
-  */
+  const data = fs.readFileSync(getDirectory,"utf8")  
+  return JSON.parse(data) ;
 }
 
-/*
-  - TODO 3: Deve retornar uma exceção de erro "User is required" caso não seja passado os dados
-  - TODO 4: Deve ser usado um método de escrita do FS como writeFile, writeFileSync, etc;   
-  - TODO 5: Deve retornar o usuário após salvar no banco de dados;
-*/
 
 async function createUserRepository(user) {
-  /*
-  - TODO 6: Deve retornar uma exceção de erro "User Id is required" caso não seja passado o ID
-  - TODO 7: Deve ser usado um método de escrita do FS como writeFile, writeFileSync, etc;  
-  - TODO 8: Deve retornar TRUE após salvar no banco de dados;
-  */
+  if (!user) {
+    throw new Error("User is required");
+  }
+  const users = await loadUsersRepository();
+  const newUsers = [...users, user]
+  fs.writeFileSync(getDirectory, JSON.stringify(newUsers));
+  return true
 }
 
+
 async function updateUserRepository(id, data) {
-  /*
-  - TODO 9: Deve retornar uma exceção de erro "User Id is required" caso não seja passado o ID
-  - TODO 10: Deve ser usado um método de escrita do FS como writeFile, writeFileSync, etc;  
-  - TODO 11: Deve retornar TRUE após remover um dado no banco de dados;
-*/
+  if(!id){
+    throw new Error("User Id is required")
+  }
+  const oldUsers = loadUsersRepository()
+
+  const newUsers = oldUsers.map((user) => {
+    if (parseInt(user.id) === id){
+      user.id = id
+      user.name = data.name || user.name
+      user.email = data.email || user.email
+      user.password = data.password || user.password
+      user.phone = data.phone || user.phone
+    }
+  })
+
+  fs.writeFileSync(getDirectory, JSON.stringify(newUsers))
+  return true
 }
 
 async function deleteUserRepository(id) {
@@ -39,6 +47,14 @@ async function deleteUserRepository(id) {
   - TODO 14: Deve retornar TRUE após remover um dado no banco de dados;
 */
 }
+
+
+
+
+// console.log(loadUsersRepository())
+// createUserRepository('1003')
+// updateUserRepository('1002', 'ola mundo')
+
 
 module.exports = {
   loadUsersRepository,
