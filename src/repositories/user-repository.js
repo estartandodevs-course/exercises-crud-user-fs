6151c9d94c5f3b2644a1b812f9c740b4ac1a0d33
Victor Almeida <victorpy.1999@gmail.com>
2022-07-04 10:28:59 -0300

    chore: Passed all tests

--- src/repositories/user-repository.js
+++ src/repositories/user-repository.js
@@ -1,9 +1,9 @@
-const fs = require('fs')
+const fs = require('fs');
 const path = require('path')
 const getDirectory = path.join(__dirname, '..', 'data', 'users.json')
 
 
-async function loadUsersRepository() {
+function loadUsersRepository() {
   const data = fs.readFileSync(getDirectory,"utf8")  
   return JSON.parse(data) ;
 }
@@ -24,36 +24,30 @@ async function updateUserRepository(id, data) {
   if(!id){
     throw new Error("User Id is required")
   }
-  const oldUsers = loadUsersRepository()
-
-  const newUsers = oldUsers.map((user) => {
-    if (parseInt(user.id) === id){
-      user.id = id
-      user.name = data.name || user.name
-      user.email = data.email || user.email
-      user.password = data.password || user.password
-      user.phone = data.phone || user.phone
+  const users = await loadUsersRepository()
+  users.forEach((user) => {
+    if (user.id === id){
+      user.id = id;
+      user.name = data.name || user.name;
+      user.email = data.email || user.email;
+      user.password = data.password || user.password;
+      user.phone = data.phone || user.phone; 
     }
-  })
-
-  fs.writeFileSync(getDirectory, JSON.stringify(newUsers))
+  });
+  fs.writeFileSync(getDirectory, JSON.stringify(users))
   return true
 }
 
+
 async function deleteUserRepository(id) {
-  /*
-  - TODO 12: Deve retornar uma exceção de erro "User Id is required" caso não seja passado o ID
-  - TODO 13: Deve ser usado um método de escrita do FS como writeFile, writeFileSync, etc;  
-  - TODO 14: Deve retornar TRUE após remover um dado no banco de dados;
-*/
+  if(!id){
+    throw new Error("User Id is required")
+  }
+  const users = await loadUsersRepository()
+  const newUsers = users.filter((user) => user.id !== id)
+  fs.writeFileSync(getDirectory, JSON.stringify(newUsers))
+  return true
 }
-
-
-
-
-// console.log(loadUsersRepository())
-// createUserRepository('1003')
-// updateUserRepository('1002', 'ola mundo')
 
 
 module.exports = {
