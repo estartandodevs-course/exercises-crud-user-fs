5ac8ad82b90720d424f5efbb0733cb7a60911b4d
Victor Almeida <victorpy.1999@gmail.com>
2022-07-05 12:30:38 -0300

    chore: encrypt-password added

--- src/services/users/create-user.service.js
+++ src/services/users/create-user.service.js
@@ -1,21 +1,23 @@
 const { createUserRepository } = require("../../repositories/user-repository.js")
 const { loadUsersRepository } = require("../../repositories/user-repository.js")
 const { findUserByEmail } = require("../users/find-user-by-email.service.js")
+const { encryptPassword } = require('../../utils/encrypt-password.js')
 
 async function createUser({ name, email, password, phone }) {
   const users = await loadUsersRepository()
 
   if (!name || !email || !password) throw new Error("Name, Email, Password are required")
 
-  if (await findUserByEmail(email) === true) {
-    return Promise.reject(new Error("User already exists"))
+  const userExist = await findUserByEmail(email)
+  if (userExist === true) {
+    throw new Error("User already exists")
   }
 
   const newUser = {
     id: Date.now(),
     name: name,
     email: email,
-    password: password,
+    password: encryptPassword(password),
     phone: phone,
     status: true
   }
